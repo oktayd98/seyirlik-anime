3fc8f456c825d2af49ac3bddb85a61289290f70c
Oktay Dönmez <oktaydonmez98@gmail.com>
2020-12-08 17:35:33 +0300

    update admin router

--- routers/admin.js
+++ routers/admin.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const adminController = require('../controllers/adminController');
+const isAuth = require('../middlewares/isAuth');
 
-router.get('/', adminController.indexPage);
+router.get('/newanime', isAuth, adminController.login);
+router.get('/newcollection', isAuth, adminController.login);
+router.get('/animes', isAuth, adminController.login);
+router.get('/collections', isAuth, adminController.login);
+router.get('/login', adminController.login);
+router.post('/login', adminController.login);
+router.post('/register', adminController.register);
 
 module.exports = router;
